a52dc19e1db75643567a09cfebb457789fcf6146
Samuel7649 <samuellareoye@outlook.com>
2026-07-31 10:34:49 +0100

    Add deadline data model

---
+++ lib/models/deadline.dart
@@ -0,0 +1,13 @@
+class Deadline {
+  const Deadline({
+    required this.title,
+    required this.course,
+    required this.date,
+    required this.status,
+  });
+
+  final String title;
+  final String course;
+  final DateTime date;
+  final String status;
+}
