70dbb7977036d431f93152b0f9a2c118f24486d3
Samuel7649 <samuellareoye@outlook.com>
2026-07-31 10:35:44 +0100

    Connect application routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:moodle/views/dashboard_view.dart';
-import 'package:moodle/views/courses_view.dart';
 import 'package:moodle/constants.dart';
+import 'package:moodle/views/assessments_view.dart';
+import 'package:moodle/views/assignment_submission_view.dart';
+import 'package:moodle/views/calendar_view.dart';
+import 'package:moodle/views/course_details_view.dart';
+import 'package:moodle/views/courses_view.dart';
+import 'package:moodle/views/dashboard_view.dart';
+import 'package:moodle/views/login_view.dart';
+import 'package:moodle/views/notifications_view.dart';
+import 'package:moodle/views/profile_view.dart';
 
 void main() {
   runApp(const MoodleApp());
@@ -14,6 +21,7 @@ class MoodleApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Moodle',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
@@ -22,13 +30,20 @@ class MoodleApp extends StatelessWidget {
           secondary: moodleSecondary,
           surface: moodleSurface,
         ),
+        scaffoldBackgroundColor: moodleBg,
       ),
       initialRoute: '/',
       routes: {
         '/': (context) => const DashboardView(),
+        '/login': (context) => const LoginView(),
         '/courses': (context) => const CoursesView(),
+        '/course-details': (context) => const CourseDetailsView(),
+        '/assessments': (context) => const AssessmentsView(),
+        '/assignment': (context) => const AssignmentSubmissionView(),
+        '/calendar': (context) => const CalendarView(),
+        '/notifications': (context) => const NotificationsView(),
+        '/profile': (context) => const ProfileView(),
       },
-      debugShowCheckedModeBanner: false,
     );
   }
 }
